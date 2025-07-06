de3402ee154ecf638f6dfaee9e817f316071250e
janaki priya <115523907+J1A2N3@users.noreply.github.com>
2025-07-06 23:19:16 +0530

    Add files via upload

---
+++ sort_array_without_duplicates.java
@@ -0,0 +1,18 @@
+package DSA_PRBLMS;
+import java.util.Arrays;
+
+public class sort_array_without_duplicates {
+	    public static void main(String[] args) {
+	        int[] arr = {4, 2, 7, 4, 7, 9, 2, 1};
+	        Arrays.sort(arr); // O(n log n)
+
+	        System.out.print(arr[0] + " "); // Print first element
+
+	        for (int i = 1; i < arr.length; i++) {
+	            if (arr[i] != arr[i - 1]) {
+	                System.out.print(arr[i] + " ");
+	            }
+	        }
+	    }
+	}
+
